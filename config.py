3b2550bb9e7250ca0a7a0a8199496d539ab68780
Rahul Kumar Ranjan <kjrahul21@gmail.com>
2024-12-26 12:36:15 +0530

    db changed

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     JWT_REFRESH_TOKEN: str = "your-jwt-refresh-secret-key"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
-    DATABASE_URL: str = "sqlite:///./finance.db"
+    DATABASE_URL: str = "postgresql://postgres:MDJiJRpYPofUEWBSkCRMRBSgPGugqSrT@autorack.proxy.rlwy.net:42994/railway"
 
     class Config:
         env_file = ".env"
